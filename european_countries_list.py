bf2a42c9e31d060d2334d436cf7ccdeff06d97dc
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-01-03 17:10:03 -0600

    Add files via upload

---
+++ european_countries_list.py
@@ -0,0 +1,50 @@
+country,x,y
+Albania
+Andorra
+Austria
+Belarus
+Belgium
+Bosnia and Herzegovina
+Bulgaria
+Croatia
+Czech Republic
+Denmark,-115,70
+Estonia
+Finland,30,200
+France
+Germany
+Greenland
+Holy See
+Hungary
+Iceland
+Ireland,-240,70
+Italy
+Kazakhstan
+Kosovo
+Latvia
+Liechtenstein
+Lithuania
+Luxembourg
+Macedonia
+Malta
+Moldova
+Monaco
+Montenegro
+Netherlands
+Norway,-85,130
+Poland
+Portugal
+Romania
+Russia,148,101
+San Marino
+Serbia
+Slovakia
+Slovenia
+Spain
+Sweden,-40,170
+Switzerland
+Turkey
+Vatican City
+Ukraine
+United Kingdom,-215,50
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("European Countries")
+image = "europe_blank_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("european_countries_list.py")
+all_countries = data.country.tolist()
+guessed_countries = []
+
+while len(guessed_countries) < 47:
+	answer_country = screen.textinput(f"{len(guessed_countries)}/47 Countries Correct",
+		prompt="What's another country's name?").title()
+
+#Upon exit check what the user missed and write a new csv file with the missed countries for them to study
+	if answer_country == "Exit":
+		missed_countries = []
+		for country in countries:
+			if country not in missed_countries:
+				missed_countries.append(country)	
+		#new_data = pandas.DataFrame(missed_countries)
+		#new_data.to_csv("countries_to_learn")
+
+		break
+
+#If the guess is correct add the country to the map using a turtle
+	if answer_country in all_countries:
+		guessed_countries.append(answer_country)
+		t=turtle.Turtle()
+		t.hideturtle()
+		t.penup()
+		country_data = data[data.country == answer_country]
+		t.goto(int(country_data.x), int(country_data.y))
+		t.write(answer_country)
+
+
+turtle.exitonclick()
