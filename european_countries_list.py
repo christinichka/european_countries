b0197f850291143b269ec3a8b947af7414167e67
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-01-04 14:44:04 -0600

    Add files via upload

--- european_countries_list.py
+++ european_countries_list.py
@@ -1,50 +1,46 @@
 country,x,y
-Albania
-Andorra
-Austria
-Belarus
-Belgium
-Bosnia and Herzegovina
-Bulgaria
-Croatia
-Czech Republic
+Albania,-25,-135
+Andorra,-205,-105
+Austria,-80,-50
+Belarus,40,30
+Belgium,-150,10
+Bosnia,-30,-115
+Bulgaria,30,-115
+Croatia,-45,-75
+Czech Republic,-70,-15
 Denmark,-115,70
-Estonia
+Estonia,30,95
 Finland,30,200
-France
-Germany
-Greenland
-Holy See
-Hungary
-Iceland
+France,-200,-40
+Germany,-110,0
+Greece,0,-155
+Hungary,-25,-55
+Iceland,-235,250
 Ireland,-240,70
-Italy
-Kazakhstan
-Kosovo
-Latvia
-Liechtenstein
-Lithuania
-Luxembourg
-Macedonia
-Malta
-Moldova
-Monaco
-Montenegro
-Netherlands
+Italy,-110,-80
+Latvia,30,75
+Liechtenstein,-100,-50
+Lithuania,15,60
+Luxembourg,-150,-5
+Malta,-95,-210
+Moldova,80,-60
+Monaco,-160,-90
+Montenegro,-45,-120
+The Netherlands,-150,20
+Macedonia,0,-125
 Norway,-85,130
-Poland
-Portugal
-Romania
+Poland,-30,10
+Portugal,-320,-110
+Romania,10,-70
 Russia,148,101
-San Marino
-Serbia
-Slovakia
-Slovenia
-Spain
-Sweden,-40,170
-Switzerland
-Turkey
-Vatican City
-Ukraine
+San Marino,-100,-90
+Serbia,-10,-95
+Slovakia,-25,-35
+Slovenia,-80,-70
+Spain,-255,-110
+Sweden,-40,180
+Switzerland,-130,-50
+Ukraine,80,-20
 United Kingdom,-215,50
+Vatican City,-110,-125
 
